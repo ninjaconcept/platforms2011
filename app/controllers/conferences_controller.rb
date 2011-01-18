367ec61eb0214801a4e1a95d0cc58d342fd76689
Florian Gilcher <florian.gilcher@asquera.de>
2011-01-18 12:58:43 +0100

    Correct show and create action for JSON

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -3,19 +3,20 @@ class ConferencesController < InheritedResources::Base
   
   def show
     @conference = Conference.find(params[:id])
-    case request.format
-    when :json
-      render :json => @conference
-    when :html
+    
+    respond_to do |format|
+      format.json { render :json => @conference }
+      format.html { render } #TODO => change
     end
   end
   
   def create
+    raise MissingData unless params[:conference]
     @conference = Conference.create(params[:conference])
         
     respond_to do |format|
       format.json { render :json => @conference }
-      format.html { redirect_to "/"} #TODO => change
+      format.html { redirect_to "/" } #TODO => change
     end
   end
 end
