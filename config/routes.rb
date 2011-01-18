7fd74e29d860680a28e92e9487a06be76890ad0f
Florian Gilcher <florian.gilcher@asquera.de>
2011-01-18 17:14:31 +0100

    Fixed attendee routes (M157-159)

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,8 @@ PlatForms::Application.routes.draw do
   
   scope "/ws", :contraints => { :format => :json }, :defaults => {:format => :json} do
     resources :conferences, :only => [:create, :show, :update] do
-      resources :attendees, :only => [:create, :index, :destroy]
+      resources :attendees, :only => [:create, :index]
+      match "attendees/:username" => "attendees#destroy", :via => :delete
     end
     resources :members, :only => [:create, :show, :update] do
       resources :contacts, :only => [:create, :index] 
