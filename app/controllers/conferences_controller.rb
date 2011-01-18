116d2df7dbc85dce4e9757827b285721beff20d6
Florian Gilcher <florian.gilcher@asquera.de>
2011-01-18 12:24:44 +0100

    Integration Specs and conferences controller

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,2 +1,21 @@
 class ConferencesController < InheritedResources::Base
+  respond_to :html, :json
+  
+  def show
+    @conference = Conference.find(params[:id])
+    case request.format
+    when :json
+      render :json => @conference
+    when :html
+    end
+  end
+  
+  def create
+    @conference = Conference.create(params[:conference])
+        
+    respond_to do |format|
+      format.json { render :json => @conference }
+      format.html { redirect_to "/"} #TODO => change
+    end
+  end
 end
--- spec/factories/conferences.rb
+++ spec/factories/conferences.rb
@@ -15,3 +15,19 @@ Factory.define :conference do |f|
   f.accomodation "MyString"
   f.howtofind "MyString"
 end
+
+Factory.define :new_conference, :class => Conference do |f|
+  f.version "MyString"
+  f.name "FunConf"
+  f.creator_user_id 1
+  f.series_id 1
+  f.start_date "2011-01-18"
+  f.end_date "2011-01-18"
+  f.description "MyString"
+  f.location "MyString"
+  f.gps_long 1.5
+  f.gps_lat 1.5
+  f.venue "MyString"
+  f.accomodation "MyString"
+  f.howtofind "MyString"
+end
