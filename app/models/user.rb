f5b405883dd1595f880244d5c71e15975740066b
Stefan Botzenhart <sb@basiszwo.com>
2011-01-18 12:47:59 +0100

    add validation for gps field M50;

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,17 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :fullname, :town, :country, :gps_lat, :gps_long
   
+  # composed_of :gps, :mapping => [%w(gps_lat), %w(gps_long)]
+  
   validates_presence_of :fullname, :username, :town, :country
   validates_uniqueness_of :username, :email
+  validates_format_of :gps, :with => /\d+(\.\d+)? ?[NnSs] ?,? ?\d+(\.\d+)? ?[EeWw]/, :allow_blank => true
+  
+  #TODO: @flo composite field hinzufügen
+  
+  # validates_format_of :gps_lat, :with => /\d+(\.\d+)?/, :allow_blank => true
+  # validates_format_of :gps_long, :with => /\d+(\.\d+)?/, :allow_blank => true
+
   
   def apply_omniauth(omniauth)
     authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
