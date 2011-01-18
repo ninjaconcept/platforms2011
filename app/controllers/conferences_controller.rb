7250df7a467b0ea9de43ef28205f08a2716a3148
Stefan Botzenhart <sb@basiszwo.com>
2011-01-18 22:50:41 +0100

    really use inherited resources in conferences controller;

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -2,14 +2,14 @@
 
 # class ConferencesController < BaseController
 class ConferencesController < InheritedResources::Base
-  # include WsAuth
-  # before_filter :ws_auth 
   
-  # before_filter :authenticate_user!, :except => :index
+  include WsAuth
+  before_filter :ws_auth
+  
+  before_filter :authenticate_user!, :except => :index
   
   respond_to :html, :json
   before_filter :load_conference, :only => [:show, :update]
-  before_filter :authenticate_user!
   
   verify :params => [:id], :only => [:show, :update]
   
@@ -26,35 +26,45 @@ class ConferencesController < InheritedResources::Base
     @conferences=ConferenceSearcher.do_find opts
   end
 
-  def create
-    @conference = Conference.new(params[:conference] || request.POST)
-    @conference.creator=current_user
-    @conference.save!
-      
-    respond_to do |format|
-      format.json { render :json => @conference }
-      # format.html { redirect_to "/" } #TODO => change
-    end
-  end
-  
   # def create
   #   @conference = Conference.new(params[:conference] || request.POST)
-  #   
-  #   create!
+  #   @conference.creator=current_user
+  #   @conference.save!
+  #     
+  #   respond_to do |format|
+  #     format.json { render :json => @conference }
+  #     # format.html { redirect_to "/" } #TODO => change
+  #   end
   # end
   
+  def create
+    @conference = Conference.new(params[:conference] || request.POST)
+    @conference.creator = current_user
+    
+    create!
+  end
   
+  # def update
+  #   @conference.attributes = (params[:conference] || request.POST)
+  #   @conference.save!
+  #   
+  #   respond_to do |format|
+  #     format.json { render :json => @conference }
+  #     format.html { render } #TODO => change
+  #   end
+  # end
+
   def update
     @conference.attributes = (params[:conference] || request.POST)
-    @conference.save!
     
-    respond_to do |format|
-      format.json { render :json => @conference }
-      format.html { render } #TODO => change
-    end
+    update!
   end
   
-  def load_conference
-    @conference = Conference.find(params[:id])
-  end
+
+  private
+
+    def load_conference
+      @conference = Conference.find(params[:id])
+    end
+    
 end
