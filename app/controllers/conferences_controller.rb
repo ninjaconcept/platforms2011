d6e3a5e75f915375fd8caaaac35a0a77ef1bc8fc
Heiko Seebach <heiko.seebach@web.de>
2011-01-18 22:22:33 +0100

    conf searcher done

--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -1,7 +1,7 @@
 #origin: GM
 
 class ConferencesController < InheritedResources::Base
-  before_filter :ws_auth 
+  #before_filter :ws_auth
   
   respond_to :html, :json
   before_filter :load_conference, :only => [:show, :update]
