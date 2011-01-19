b6e06e364b3f27444d5453412b7973efebe4f798
Stefan Botzenhart <sb@basiszwo.com>
2011-01-19 14:10:24 +0100

    add some css; only render root categories on start page;

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,6 @@
 class StatusController < ApplicationController
   def index
+    
     all_rcds=RcdStatus.where("(inviter_user_id=? OR invitee_user_id=?)",current_user.id,current_user.id)
     all_rcds_as_inviter=RcdStatus.where("(inviter_user_id=?)",current_user.id)
     all_rcds_as_invitee=RcdStatus.where("(invitee_user_id=?)",current_user.id)
@@ -8,5 +9,8 @@ class StatusController < ApplicationController
     @received_rcds=all_rcds_as_invitee.where("status='sent'")
     @attending_conferences=Conference.where("attendances.user_id=?",current_user.id).includes(:attendances)
     @notifications=current_user.notifications.where("`read`=?",false)
+    
+    
+    
   end
 end
