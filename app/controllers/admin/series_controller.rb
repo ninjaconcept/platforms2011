a7222ad9d1e556b570b4799c67ca132b85554950
Stefan Botzenhart <sb@basiszwo.com>
2011-01-19 15:45:56 +0100

    edit/destroy series if in official contacts; add browse series path to menu;

--- app/controllers/admin/series_controller.rb
+++ app/controllers/admin/series_controller.rb
@@ -1,3 +1,9 @@
 class Admin::SeriesController < Admin::BaseController
-  
+  def update
+    update! do |success, failure|
+      success.html { 
+        redirect_to admin_series_index_path 
+      }
+    end
+  end
 end
