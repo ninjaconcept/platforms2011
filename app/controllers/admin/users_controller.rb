177365ab934f609e226e3b66efab9faf2d12a942
Stefan Botzenhart <sb@basiszwo.com>
2011-01-19 16:08:00 +0100

    add admin search for users (S141);

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,3 +1,14 @@
 class Admin::UsersController < Admin::BaseController
   
+  def index
+    @search_term = params[:user][:search_term] if params[:user]
+    term = "%#{@search_term}%"
+
+    @users = User.where("username LIKE ? OR email LIKE ? OR fullname LIKE ?", term, term, term)
+    
+    logger.debug "Search: #{params[:user][:search_term]}" if params[:user]
+    
+    index!
+  end
+  
 end
